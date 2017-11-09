7135a30913c57432a5b84748d4bcf9cb602f701f
Murphy, Stephen - William S <William.S.Murphy@Lowes.com>
2017-11-09 14:37:06 -0500

    Moved completely to okhttp3 from Volley. Removed unused gradle dependancies

--- app/src/main/java/com/test/murphy/weatherapp/Connections.java
+++ app/src/main/java/com/test/murphy/weatherapp/Connections.java
@@ -2,12 +2,6 @@ package com.test.murphy.weatherapp;
 
 import android.content.Context;
 
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.Volley;
 import com.test.murphy.weatherapp.model.Units;
 import com.test.murphy.weatherapp.model.WeatherConditions;
 import com.test.murphy.weatherapp.model.WeatherForecast;
@@ -29,16 +23,15 @@ import okhttp3.ResponseBody;
 
 public class Connections {
 
-    private static ConnectionsDelegate delegate;
-
     private static Connections mInstance;
 
     private Context mContext;
-    private RequestQueue mQueue;
+    private OkHttpClient client = new OkHttpClient();
+    private ConnectionsDelegate delegate;
+
 
     public Connections(final Context context) {
         mContext = context;
-        mQueue = getRequestQueue();
     }
 
     public static Connections getInstance(final Context context) {
@@ -54,67 +47,48 @@ public class Connections {
         this.delegate = delegate;
     }
 
-    public RequestQueue getRequestQueue(){
-        // If RequestQueue is null the initialize new RequestQueue
-        if(mQueue == null){
-            mQueue = Volley.newRequestQueue(mContext.getApplicationContext());
-        }
-
-        // Return RequestQueue
-        return mQueue;
-    }
-
-
     public void getWeather(String zip, Units units) {
-        String BASE_URL = "http://api.openweathermap.org/data/2.5/weather?zip=";
         String location = zip + ",us";
-        String appId = "&APPID=b4608d4fcb4accac0a8cc2ea6949eeb5";
-        String unitsPart = "&units=" + units.getType();
 
-        String url = BASE_URL + location + appId + unitsPart;
+        HttpUrl.Builder builder = new HttpUrl.Builder();
+        builder.scheme("http")
+                .host("api.openweathermap.org")
+                .addPathSegment("data")
+                .addPathSegment("2.5")
+                .addPathSegment("weather")
+                .addQueryParameter("zip", location)
+                .addQueryParameter("units", units.getType())
+                .addQueryParameter("APPID","b4608d4fcb4accac0a8cc2ea6949eeb5");
 
-        JsonObjectRequest jsonRequest = new JsonObjectRequest
-                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
-                    @Override
-                    public void onResponse(JSONObject response) {
-                        WeatherConditions weather = new WeatherConditions(response);
+        final okhttp3.Request request = new okhttp3.Request.Builder()
+                .url(builder.build())
+                .header("Accept", "application/json")
+                .build();
+
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override public void onResponse(Call call, okhttp3.Response response) throws IOException {
+                try (ResponseBody responseBody = response.body()) {
+                    if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
+
+                    try {
+                        JSONObject jsonObject = new JSONObject(responseBody.string());
+                        WeatherConditions weather = new WeatherConditions(jsonObject);
                         delegate.weatherSuccess(weather);
+                    } catch (JSONException e) {
+                        //TODO
                     }
-                }, new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {}
-                });
-
-        // Add the request to the RequestQueue.
-        mQueue.add(jsonRequest);
+                }
+            }
+        });
     }
 
-//    public void getForecastVolley(String zip, Units units) {
-//        String BASE_URL = "http://api.openweathermap.org/data/2.5/forecast?zip=";
-//        String location = zip + ",us";
-//        String appId = "&APPID=b4608d4fcb4accac0a8cc2ea6949eeb5";
-//        String unitsPart = "&units=" + units.getType();
-//
-//        String url = BASE_URL + location + appId + unitsPart;
-//
-//        JsonObjectRequest jsonRequest = new JsonObjectRequest
-//                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
-//                    @Override
-//                    public void onResponse(JSONObject response) {
-//                        WeatherForecast forecast = new WeatherForecast(response);
-//                        delegate.forecastSuccess(forecast);
-//                    }
-//                }, new Response.ErrorListener() {
-//                    @Override
-//                    public void onErrorResponse(VolleyError error) {}
-//                });
-//
-//        // Add the request to the RequestQueue.
-//        mQueue.add(jsonRequest);
-//    }
 
-
-    public void getForecastOkHttp(String zip, Units units) throws IOException {
+    public void getForecast(String zip, Units units) throws IOException {
         String location = zip + ",us";
 
         final OkHttpClient client = new OkHttpClient();
